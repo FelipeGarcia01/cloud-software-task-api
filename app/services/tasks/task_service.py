88fd2df82927cdc85a255357ddfc38a794fc2430
felipe <lf.garciar1@uniandes.edu.co>
2023-05-18 00:54:49 -0500

    add close session

--- app/services/tasks/task_service.py
+++ app/services/tasks/task_service.py
@@ -1,3 +1,4 @@
+import logging
 import uuid
 from app.models.models import Task, GetTaskSchema, File, PostTaskSchema, GetTaskByIdSchema
 from app.databases import db
@@ -13,48 +14,62 @@ get_task_by_id_schema = GetTaskByIdSchema()
 class TaskService:
     @classmethod
     def get_task(cls, id_user, max_tasks, order_tasks):
-        type_order = OrderEnum.asc if order_tasks is None or order_tasks == '0' else OrderEnum.desc
-        if not max_tasks:
-            result_query = Task.query.filter(Task.id_user == id_user)\
-                .order_by(type_order(Task.id)).all()
-        else:
-            result_query = Task.query.filter(Task.id_user == id_user)\
-                .order_by(type_order(Task.id)).limit(max_tasks).all()
+        try:
+            type_order = OrderEnum.asc if order_tasks is None or order_tasks == '0' else OrderEnum.desc
+            if not max_tasks:
+                result_query = Task.query.filter(Task.id_user == id_user)\
+                    .order_by(type_order(Task.id)).all()
+            else:
+                result_query = Task.query.filter(Task.id_user == id_user)\
+                    .order_by(type_order(Task.id)).limit(max_tasks).all()
 
-        return [task_schema.dump(task) for task in result_query]
+            return [task_schema.dump(task) for task in result_query]
+        except Exception as e:
+            logging.error("fail getting task with error: {error}".format(error=e))
+        finally:
+            db.session.close()
 
     @classmethod
     def post_task(cls, id_user, name_file, file_data, new_format):
-        file_server = FileService()
-        pub_sub_topic = PubSubTopic()
+        try:
+            file_server = FileService()
+            pub_sub_topic = PubSubTopic()
 
-        temp_name = name_file.split('.')
-        id_file_upload = file_server.send_file(name_file, file_data, new_format)
-        new_task = Task(
-            id_user=id_user,
-            id_original_file=id_file_upload,
-            file_name=temp_name[0],
-            original_extension=temp_name[1],
-            new_extension=new_format,
-            status="uploaded",
-            task_id=str(uuid.uuid4())
-        )
-        db.session.add(new_task)
-        db.session.commit()
-        task_id = new_task.task_id
-        url = id_file_upload
-        msg = dict(task_id=task_id, url=url, new_format=new_format)
-        pub_sub_topic.send_message(msg)
-        return post_schema.dump(new_task)
+            temp_name = name_file.split('.')
+            id_file_upload = file_server.send_file(name_file, file_data, new_format)
+            new_task = Task(
+                id_user=id_user,
+                id_original_file=id_file_upload,
+                file_name=temp_name[0],
+                original_extension=temp_name[1],
+                new_extension=new_format,
+                status="uploaded",
+                task_id=str(uuid.uuid4())
+            )
+            db.session.add(new_task)
+            db.session.commit()
+            task_id = new_task.task_id
+            url = id_file_upload
+            msg = dict(task_id=task_id, url=url, new_format=new_format)
+            pub_sub_topic.send_message(msg)
+            return post_schema.dump(new_task)
+        except Exception as e:
+            logging.error("fail creating task: {task}, whit error: {error}".format(task=name_file+new_format, error=e))
+        finally:
+            db.session.close()
 
     @classmethod
     def get_task_by_id(cls, id_task):
-        result_query = Task.query.filter(Task.task_id == id_task).first()
-        return get_task_by_id_schema.dump(result_query)
+        try:
+            result_query = Task.query.filter(Task.task_id == id_task).first()
+            return get_task_by_id_schema.dump(result_query)
+        except Exception as e:
+            logging.error("fail getting task: {task}, with error: {error}".format(task=id_task, error=e))
+        finally:
+            db.session.close()
 
     @classmethod
     def delete_task_by_id(cls, id_task):
-        result = False
         try:
             task = Task.query.filter(Task.task_id == id_task).first()
             if task and task.status.lower() == 'processed':
@@ -64,7 +79,9 @@ class TaskService:
                 if original_file:
                     db.session.delete(original_file)
                     db.session.commit()
-                result = True
-        except KeyError:
-            result = False
-        return result
+                return True
+        except KeyError as e:
+            logging.error("KeyError on delete task: {task}, with error: {error}".format(task=id_task, error=e))
+            return False
+        finally:
+            db.session.close()
